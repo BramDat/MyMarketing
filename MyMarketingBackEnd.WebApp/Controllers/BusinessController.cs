8beccda8aafacfae57d142923320643adc38db6e
Brahmadath Mampazhassery <brahmadathsm@outlook.com>
2017-09-26 09:27:13 +0530

    changes to gallery

---
+++ MyMarketingBackEnd.WebApp/Controllers/BusinessController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MyMarketingBackEnd.WebApp.Controllers
+{
+    public class BusinessController : Controller
+    {
+        public ActionResult Index()
+        {
+            // Get the list of businesses
+            return View();
+        }
+
+        public ActionResult EditBusiness(string businessId)
+        {
+            return View();
+        }
+
+    }
+}
